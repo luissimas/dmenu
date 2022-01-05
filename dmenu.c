129c2f6f614ea12f46f3a2fbc76d1b7bad924fe9
padawan <luissimas@protonmail.com>
2022-01-05 11:22:07 -0300

    Lines below prompt

--- dmenu.c
+++ dmenu.c
@@ -205,7 +205,7 @@ static void drawmenu(void) {
   if (lines > 0) {
     /* draw vertical list */
     for (item = curr; item != next; item = item->right)
-      drawitem(item, x, y += bh, mw - x);
+      drawitem(item, x - promptw, y += bh, mw);
   } else if (matches) {
     /* draw horizontal list */
     x += inputw;
